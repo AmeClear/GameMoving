e760504b54da536e58aa61f040074858ff1b3a41
c1earF <clearfnew@gmail.com>
2023-12-26 17:39:54 +0800

    加物理坡度

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    // [SerializeField]
-    // Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
     [SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)]
-    float maxAcceleration = 10f;
+    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+
     [SerializeField, Range(0f, 1f)]
-    // float bounciness = 0.5f;
+    float bounciness = 0.5f;
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+    [SerializeField, Range(0f, 90f)]
+    float maxGroundAngle = 25f;
+    float minGroundDotProduct;
+    int jumpPhase;
+    bool desiredJump = false;
+    bool onGround;
     /// <summary>
     /// 实际速度
     /// </summary> 
@@ -21,43 +32,82 @@ public class MovingSphere : MonoBehaviour
     /// </summary>
     Vector3 desiredVelocity;
     Vector2 playerInput;
+    Rigidbody body;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        OnValidate();
+    }
+    void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
     void Update()
     {
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        // playerInput.Normalize();
-        // playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        // transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
         desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredJump |= Input.GetKeyDown(KeyCode.Space);
+    }
+    private void FixedUpdate()
+    {
+        UpdateState();
+
         //加速度
-        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.deltaTime;
         velocity.x =
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z =
             Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        Vector3 displacement = velocity * Time.deltaTime;
-        // transform.localPosition += displacement;
-        Vector3 newPosition = transform.localPosition + displacement;
-        // if (newPosition.x < allowedArea.xMin)
-        // {
-        //     newPosition.x = allowedArea.xMin;
-        //     velocity.x = -velocity.x * bounciness;
-        // }
-        // else if (newPosition.x > allowedArea.xMax)
-        // {
-        //     newPosition.x = allowedArea.xMax;
-        //     velocity.x = -velocity.x * bounciness;
-        // // }
-        // if (newPosition.z < allowedArea.yMin)
-        // {
-        //     newPosition.z = allowedArea.yMin;
-        //     velocity.z = -velocity.z * bounciness;
-        // }
-        // else if (newPosition.z > allowedArea.yMax)
-        // {
-        //     newPosition.z = allowedArea.yMax;
-        //     velocity.z = -velocity.z * bounciness;
-        // }
-        transform.localPosition = newPosition;
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+        body.velocity = velocity;
+        onGround = false;
+    }
+    void UpdateState()
+    {
+        velocity = body.velocity;
+        if (onGround)
+        {
+            jumpPhase = 0;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //onGround = true;
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        //onGround = true;
+        EvaluateCollision(collision);
+    }
+
+
+    void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= minGroundDotProduct;
+        }
+    }
+    void Jump()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            if (velocity.y > 0f)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+            }
+            velocity.y += jumpSpeed;
+        }
     }
 }
